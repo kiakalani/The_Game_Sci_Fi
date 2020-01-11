4acc37e786e1364922380c1d993c673bc6ebb936
Kia <kalkia2000@gmail.com>
2020-01-11 15:45:04 -0500

    The stuff

---
+++ src/DefaultScreen.java
@@ -0,0 +1,15 @@
+import javafx.scene.Group;
+import javafx.scene.Scene;
+
+public abstract class DefaultScreen extends Group {
+    private final Scene scene = new Scene(this,Run.relativeX(100),Run.relativeY(100));
+    private double relativeX(double x) {
+        return Run.relativeX(x);
+    }
+    private double relativeY(double y) {
+        return Run.relativeY(y);
+    }
+    public DefaultScreen() {
+
+    }
+}
--- src/MainMenu.java
+++ src/MainMenu.java
@@ -3,9 +3,6 @@ import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
-public class MainMenu extends Group {
-    private final Scene scene= new Scene(this);
-    public MainMenu() {
-        getChildren().add(new Rectangle(Run.relativeX(90),Run.relativeY(90), Color.RED));
-    }
+public class MainMenu extends DefaultScreen {
+
 }
